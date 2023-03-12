d6ba6b4d87393c2783f9e1620af390a87dcdc1c9
root <root@ciscoMachine>
2023-03-12 14:53:01 +0100

    parallel initial

---
+++ parallel-cpp/tsp-omp.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+#include "queue.hpp"
+#include "element.h"
+
+void parse_inputs(int argc, char *argv[]);
+vector<pair<double,double>> get_mins();
+double initialLB(vector<pair<double,double>> &mins);
+double calculateLB(vector<pair<double,double>> &mins, int f, int t, double LB);
+void create_children(QueueElem &myElem, PriorityQueue<QueueElem> &myQueue, vector<pair<double,double>> &mins);
+pair<vector<int>, double> tsp_parallel();
+void print_result(vector <int> BestTour, double BestTourCost);
+
+// global variables
+double BestTourCost;
+int numCities, numRoads;
+vector <vector <double>> distances;
