16e932239d223ce8afc450343214f92e27625a71
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-04-07 23:12:35 +0100

    started tsp-mpi

--- MPI/tsp-mpi.h
+++ MPI/tsp-mpi.h
@@ -15,3 +15,8 @@ using namespace std;
 void parse_inputs(int argc, char *argv[]);
 void print_result(vector <int> BestTour, double BestTourCost);
 pair<vector <int>, double> tsp();
+
+// global variables
+double BestTourCost;
+int numCities, numRoads;
+vector <vector <double>> distances;
