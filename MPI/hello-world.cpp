903da5b56113d9fab1b6f05889760048b44fa0d7
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-04-11 22:48:25 +0100

    test hello world file

--- MPI/hello-world.cpp
+++ MPI/hello-world.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <mpi.h>
 
-int main() {
-	std::cout << "Hello world!" << std::endl;
-	return 0;
+int main(int argc, char** argv) {
+    int rank, size;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    printf("Process %d started\n", rank);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    printf("Process %d passed the barrier\n", rank);
+	
+    MPI_Finalize();
+    return 0;
 }
